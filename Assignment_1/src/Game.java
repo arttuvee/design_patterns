0d3d4cac08de6475bb982247d01f610c9ea30152
arttuvee <arttuvo@metropolia.fi>
2024-08-28 14:32:44 +0300

    Pieni korjaus

--- Assignment_1/src/Game.java
+++ Assignment_1/src/Game.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter map type: ");
+        String type = scanner.nextLine();
+
         Game game = new Game();
         Map map = game.createMap("Wilderness", 10, 5);
         map.display();
